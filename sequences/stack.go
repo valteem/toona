ad9508d62650379c72e164023624c1112eacdee4
valteem <val.teem.scn@gmail.com>
2023-09-08 20:49:32 +0300

    added stack ADT with pre-allocated max depth

--- sequences/stack.go
+++ sequences/stack.go
@@ -5,6 +5,11 @@ import (
 	"sync"
 )
 
+var (
+	errStackEmpty = errors.New("stack empty")
+	errMaxDepth = errors.New("max stack depth achieved")
+)
+
 type Stack [T any] struct {
 	lock sync.Mutex
 	arr []T
@@ -29,11 +34,52 @@ func (s *Stack[T]) Push(e T) {
 func (s *Stack[T]) Pop() (T, error) {
 	if s.IsEmpty() {
 		var t T // returning 'zero' T value
-		return t, errors.New("stack empty")
+		return t, errStackEmpty
 	}
 	s.lock.Lock()
 	defer s.lock.Unlock()
 	r := s.arr[len(s.arr) - 1]
 	s.arr = s.arr[:len(s.arr) - 1]
+	return r, nil
+}
+
+type AllocStack [T any] struct {
+	lock sync.Mutex
+	arr []T
+	depth int
+	maxDepth int
+}
+
+func NewAllocStack[T any](maxDepth int) *AllocStack[T] {
+	return &AllocStack[T]{sync.Mutex{}, make([]T, maxDepth), 0, maxDepth}
+}
+
+func (a *AllocStack[T]) IsEmpty() bool {
+	a.lock.Lock()
+	defer a.lock.Unlock()
+	return a.depth == 0
+}
+
+func (a *AllocStack[T]) Push(e T) error {
+	a.lock.Lock()
+	defer a.lock.Unlock()
+	if a.depth == a.maxDepth {
+		return errMaxDepth
+	} else {
+		a.depth++
+		a.arr[a.depth - 1] = e
+		return nil
+	}
+}
+
+func (a *AllocStack[T]) Pop() (T, error) {
+	if a.IsEmpty() {
+		var t T // returning 'zero' T value
+		return t, errStackEmpty
+	}
+	a.lock.Lock()
+	defer a.lock.Unlock()
+	r := a.arr[a.depth - 1]
+	a.depth--
 	return r, nil
 }
--- sequences/stack_test.go
+++ sequences/stack_test.go
@@ -13,3 +13,23 @@ func TestStack(t *testing.T) {
 		t.Error("wrong Pop() result")
 	}
 }
+
+func TestAllocStack(t *testing.T) {
+	
+	s := sequences.NewAllocStack[int](10)
+	
+	s.Push(1)
+	if v, _ := s.Pop(); v != 1 {
+		t.Error("wrong Pop() result")
+	}
+
+	for i := 1; i <= 10; i++ {
+		if e := s.Push(i); e != nil {
+			t.Error("wrong Push() result - max stack depth not achieved yet")
+		}
+	}
+	if e := s.Push(11); e == nil {
+		t.Error("wrong Push() result - max stack depth achieved error not thrownd")
+	}
+	
+}
