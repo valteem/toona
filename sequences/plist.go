608d5a536522990d83206b445f134a0927f1c667
valteem <val.teem.scn@gmail.com>
2023-09-18 13:18:28 +0300

    complete positional list implementation with tests

--- sequences/plist.go
+++ sequences/plist.go
@@ -10,6 +10,8 @@ import (
 var (
 	ErrWrongCont = fmt.Errorf("position does not belong to the list")
 	ErrOutOfBoundOrRemoved = fmt.Errorf(("position out of list boundaries or removed from list"))
+	ErrPListEmpty = fmt.Errorf("positional list empty")
+	ErrPListEmptyInsert = fmt.Errorf("positional list empty, only InsertToTail()/InsertToHead operations available")
 )
 
 type Position [T any] struct {
@@ -38,8 +40,9 @@ func NewPList[T any]() *PList[T] {
 }
 
 func (l *PList[T]) validate(pos *Position[T]) (*Node[T], error) {
-	l.lock.Lock()
-	defer l.lock.Unlock()
+// lock/unlock is take care of by the caller
+	// l.lock.Lock()
+	// defer l.lock.Unlock()
 	if pos.cont != l.list {
 		return nil, ErrWrongCont
 	}
@@ -50,20 +53,64 @@ func (l *PList[T]) validate(pos *Position[T]) (*Node[T], error) {
 }
 
 func (l *PList[T]) position(node *Node[T]) *Position[T] {
-	l.lock.Lock()
-	defer l.lock.Unlock()
+// lock/unlock is take care of by the caller
+	// l.lock.Lock()
+	// defer l.lock.Unlock()
 	if node == l.list.tail || node == l.list.head {
 		return nil
 	}
 	return NewPosition[T](l.list, node)
 }
 
-func (l *PList[T]) Head() *Position[T] {
+func (l *PList[T]) Head() (*Position[T], error) {
 	l.lock.Lock()
 	defer l.lock.Unlock()
-	return NewPosition[T](l.list, l.list.head.Previous)
+	if l.list.size == 0 {
+		var p *Position[T]
+		return p, ErrPListEmpty
+	}
+	return NewPosition[T](l.list, l.list.head.Previous), nil
 }
 
+func (l *PList[T]) Tail() (*Position[T], error) {
+	l.lock.Lock()
+	defer l.lock.Unlock()
+	if l.list.size == 0 {
+		var p *Position[T]
+		return p, ErrPListEmpty
+	}
+	return NewPosition[T](l.list, l.list.tail.Next), nil
+}
+
+func (l *PList[T]) Before(pos *Position[T]) (*Position[T], error) {
+	l.lock.Lock()
+	defer l.lock.Unlock()
+	if l.list.size == 0 {
+		var p *Position[T]
+		return p, ErrPListEmpty
+	}
+	n, e := l.validate(pos)
+	if e != nil {
+		var p *Position[T]
+		return p, e
+	}
+	return l.position(n.Previous), nil
+}
+
+func (l *PList[T]) After(pos *Position[T]) (*Position[T], error) {
+	l.lock.Lock()
+	defer l.lock.Unlock()
+	if l.list.size == 0 {
+		var p *Position[T]
+		return p, ErrPListEmpty
+	}
+	n, e := l.validate(pos)
+	if e != nil {
+		var p *Position[T]
+		return p, e
+	}
+	return l.position(n.Next), nil
+}
 func (l *PList[T]) insert(elt T, prev *Node[T], next *Node[T]) *Position[T] {
 	n := l.list.Insert(elt, prev, next)
 	return NewPosition[T](l.list, n)
@@ -73,4 +120,64 @@ func (l *PList[T]) InsertToHead(elt T) *Position[T] {
 	l.lock.Lock()
 	defer l.lock.Unlock()
 	return l.insert(elt, l.list.head.Previous, l.list.head)
+}
+
+func (l *PList[T]) InsertToTail(elt T) *Position[T] {
+	l.lock.Lock()
+	defer l.lock.Unlock()
+	return l.insert(elt, l.list.tail, l.list.tail.Next)
+}
+
+func (l *PList[T]) InsertBefore(elt T, pos *Position[T]) (*Position[T], error) {
+	l.lock.Lock()
+	defer l.lock.Unlock()
+	if l.list.size == 0 {
+		var p *Position[T]
+		return p, ErrPListEmptyInsert
+	}
+	node, err := l.validate(pos)
+	if err != nil {
+		var p *Position[T]
+		return p, err
+	}
+	return l.insert(elt, node.Previous, node), nil
+}
+
+func (l *PList[T]) InsertAfter(elt T, pos *Position[T]) (*Position[T], error) {
+	l.lock.Lock()
+	defer l.lock.Unlock()
+	if l.list.size == 0 {
+		var p *Position[T]
+		return p, ErrPListEmptyInsert
+	}
+	node, err := l.validate(pos)
+	if err != nil {
+		var p *Position[T]
+		return p, err
+	}
+	return l.insert(elt, node, node.Next), nil
+}
+
+func (l *PList[T]) Remove(pos *Position[T]) (T, error) {
+	l.lock.Lock()
+	defer l.lock.Unlock()
+	node, err := l.validate(pos)
+	if err != nil {
+		var t T
+		return t, err
+	}
+	return l.list.Remove(node), nil
+}
+
+func (l *PList[T]) Replace(elt T, pos *Position[T]) (T, error) {
+	l.lock.Lock()
+	defer l.lock.Unlock()
+	node, err := l.validate(pos)
+	if err != nil {
+		var t T
+		return t, err
+	}
+	r := node.Element
+	node.Element = elt
+	return r, nil
 }
--- sequences/plist_test.go
+++ sequences/plist_test.go
@@ -1,6 +1,7 @@
 package sequences_test
 
 import (
+	"reflect"
 	"testing"
 
 	"my.play.go/toona/sequences"
@@ -12,5 +13,72 @@ func TestBasic(t *testing.T) {
 	p := l.InsertToHead(1)
 	if e := p.Element(); e != 1 {
 		t.Errorf("wrong InsertTohead result: expect %v, get %v", 1, e)
+	}
+}
+
+func TestAfter(t *testing.T) {
+	l := sequences.NewPList[string]()
+	v := []string{"apple", "pear", "cherry", "plum", "berry"}
+	for _, e := range v {
+		_ = l.InsertToHead(e)
+	}
+	p, _ := l.Tail()
+	for _, e := range v {
+		if r := p.Element(); !reflect.DeepEqual(r, e) {
+			t.Errorf("wrong node value: get %+v, expect %+v", r, e)
+		}
+		p, _ = l.After(p)
+	}	
+}
+
+func TestBefore(t *testing.T) {
+	l := sequences.NewPList[string]()
+	v := []string{"apple", "pear", "cherry", "plum", "berry"}
+	for _, e := range v {
+		_ = l.InsertToTail(e)
+	}
+	p, _ := l.Head()
+	for _, e := range v {
+		if r := p.Element(); !reflect.DeepEqual(r, e) {
+			t.Errorf("wrong node value: get %+v, expect %+v", r, e)
+		}
+		p, _ = l.Before(p)
+	}	
+}
+
+func TestInsertBeforeAfter(t *testing.T) {
+	l := sequences.NewPList[string]()
+	pos := l.InsertToTail("apple")
+	pos, _ = l.InsertBefore("pear", pos)
+	_, _ = l.InsertAfter("plum", pos)
+	v := []string{"pear", "plum", "apple"}
+	pos, _ = l.Tail()
+	for _, e := range v {
+		if r := pos.Element(); !reflect.DeepEqual(r, e) {
+			t.Errorf("wrong InsertBefore/InsertAfter result: receive %+v, expect %+v", r, e)
+		}
+		pos, _ = l.After(pos)
+	}
+}
+
+func TestRemoveReplace(t *testing.T) {
+	l := sequences.NewPList[string]()
+	v := []string{"apple", "pear", "cherry", "berry", "plum"}
+	for _, e := range v {
+		l.InsertToHead(e)
+	}
+	pos, _ := l.Tail()
+	pos, _ = l.After(pos)
+	l.Remove(pos)
+	pos, _ = l.Head()
+	pos, _ = l.Before(pos)
+	_, _ = l.Replace("onion", pos)
+	v = []string{"apple", "cherry", "onion", "plum"}
+	pos, _ = l.Tail()
+	for _, e := range v {
+		if r := pos.Element(); !reflect.DeepEqual(r, e) {
+			t.Errorf("wrong Remove/Replace result: receive %+v, expect %+v", r, e)
+		}
+		pos, _ = l.After(pos)
 	}
 }
